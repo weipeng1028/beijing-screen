22cf107bf92833d81ad8f965bcc3b60389323049
mc199645 <495750028@qq.com>
2020-09-11 18:06:23 +0800

    修改文字

--- public/config.js
+++ public/config.js
@@ -1,5 +1,11 @@
+/*
+ * @Author: MaiChao
+ * @Date: 2020-04-17 11:08:44
+ * @LastEditors: MaiChao
+ * @LastEditTime: 2020-09-11 18:04:33
+ */
 window.g = {
-  baseUrl: 'http://39.101.190.24/bigscreen',
-  baseRoute: 'http://39.101.190.24',
-  web: '39.101.190.24/bigscreen'
+  baseUrl: 'http://219.141.234.225:81/bigscreen',
+  baseRoute: 'http://219.141.234.225:81',
+  web: '219.141.234.225:81/bigscreen'
 }
